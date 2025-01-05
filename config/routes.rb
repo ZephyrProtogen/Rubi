a8dbeb3242e1d4746b2898f1b4a302e2fe10ad3a
ZephyrProtogen <jk721213707@gmail.com>
2025-01-05 12:09:22 +0100

    strony statyczne :skull: :sweat_drops:

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get "pages/about"
+  get "pages/home"
+  get "pages/contact"
+  get "about/new"
+  get "about/create"
   get "pages/home"
   get "pages/contact"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
